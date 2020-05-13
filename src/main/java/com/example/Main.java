9cf358c2c6aa2510ea5d014dbf09e72c1606df12
Brad <Brad@continuum.lan>
2020-05-13 17:24:54 -0500

    - minor refactoring in main

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -9,6 +9,7 @@ import static com.example.BinaryIndexedTree.maxValueOf;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,24 +20,38 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        List<Integer> list = fetchData();
+        int[] arr = integerListToIntArray(list);
+        Fenwick f = new Fenwick(maxValueOf(arr));
+        f.addInstances(arr);
+        dumpFenwick(f);
+    }
+
+    private static List<Integer> fetchData() throws NumberFormatException, IOException {
+        List<Integer> list = new ArrayList<>();
         try (BufferedReader r = new BufferedReader(new FileReader(new File("Wob_Valid_case2.csv")))) {
             String line;
-            List<Integer> list = new ArrayList<>();
             while ((line = r.readLine()) != null) {
                 list.add((int) (Double.parseDouble(line) * 10));
             }
-            int[] arr = new int[list.size()];
-            for (int i = 0; i < list.size(); i++) {
-                arr[i] = list.get(i);
-            }
-            Fenwick f = new Fenwick(maxValueOf(arr));
-            f.addInstances(arr);
-            boolean foundfirstNonZeroEntry = false;
-            for (int idx = 0; idx < f.tree.length; idx++) {
-                if (f.get(idx) != 0 || foundfirstNonZeroEntry) {
-                    System.out.printf("%5.1f %5d %5d %n", (idx / 10.0), f.get(idx), f.getCum(idx));
-                    foundfirstNonZeroEntry = true;
-                }
+        }
+        return list;
+    }
+
+    private static int[] integerListToIntArray(List<Integer> list) {
+        int[] arr = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+
+    private static void dumpFenwick(Fenwick f) {
+        boolean foundfirstNonZeroEntry = false;
+        for (int idx = 0; idx < f.tree.length; idx++) {
+            if (f.get(idx) != 0 || foundfirstNonZeroEntry) {
+                System.out.printf("%5.1f %5d %5d %n", (idx / 10.0), f.get(idx), f.getCum(idx));
+                foundfirstNonZeroEntry = true;
             }
         }
     }
